aa44aa60f84875c1817e02e79c89bc0f40dd0857
Vasily Boldyrev <vasil_001@mail.ru>
2022-04-14 02:43:46 +0300

    add template

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,58 @@
+const initialCards = [
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+
+const listCard = document.querySelector(".elements");
+const template = document.querySelector(".template");
+
+function render() {
+  const html = initialCards.map(getElement);
+  listCard.append(...html);
+}
+
+function getElement(item) {
+  const getElementTemplate = template.content.cloneNode(true);
+  const nameCard = getElementTemplate.querySelector(".element__title");
+  const imageCard = getElementTemplate.querySelector(".element__image");
+  nameCard.textContent = item.name;
+  imageCard.src = item.link;
+  imageCard.alt = item.name;
+  return getElementTemplate;
+}
+
+render();
+
 const editButtonProfile = document.querySelector(".profile__edit-button");
 const modalWindowProfile = document.querySelector(".popup__edit_profile");
-const modalProfileCloseButton = modalWindowProfile.querySelector(".popup__close");
+const modalProfileCloseButton =
+  modalWindowProfile.querySelector(".popup__close");
 const addCardButton = document.querySelector(".profile__add-button");
 const modalWindowAddCard = document.querySelector(".popup__add_card");
-const modalAddCardCloseButton = modalWindowAddCard.querySelector(".popup__close");
+const modalAddCardCloseButton =
+  modalWindowAddCard.querySelector(".popup__close");
 
 function toggleModalWindowAddCard() {
   modalWindowAddCard.classList.toggle("popup_is-active");
